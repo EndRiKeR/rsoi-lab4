69576745a4eefb6740875c0ebdca494f3b1820ba
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2025-12-05 01:21:20 +0300

    return back

--- services/GatewayService/Controllers/HealthController.cs
+++ services/GatewayService/Controllers/HealthController.cs
@@ -69,7 +69,7 @@ namespace GatewayService.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(200, new
+                return StatusCode(503, new
                 {
                     status = "Unhealthy",
                     timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
